4fd30f3b424d0b8131541f21b4c2c8e99cae7f11
sadat.malik <sadat.malik@me.com>
2022-04-01 09:09:08 +0100

    enable eureka server and bootstrap properties

--- src/main/java/com/sadatmalik/optima/eureka/EurekaServerApplication.java
+++ src/main/java/com/sadatmalik/optima/eureka/EurekaServerApplication.java
@@ -2,8 +2,15 @@ package com.sadatmalik.optima.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * The @EnableEurekaServer annotation enables the Eureka Server in the Spring service.
+ *
+ * @author sadatmalik
+ */
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
